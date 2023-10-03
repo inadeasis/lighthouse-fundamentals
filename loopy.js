2aaaa02333171cc9b761351299d71f29436ba09e
Ina Ricci <ina_ricci@hotmail.com>
2023-10-03 03:40:19 -0400

    Work done so far

---
+++ loopy.js
@@ -0,0 +1,13 @@
+
+// Write a loop that prints out all numbers from 100 to 200
+for (let i = 100; i <= 200; i++){
+  if (i % 3 === 0 && i % 4 === 0){
+    console.log("LoopyLighthouse");
+  } else if ( i % 4 === 0) {
+    console.log("Lighthouse")
+  } else if ( i % 3 === 0){
+    console.log("Loopy")
+  } else {
+    console.log(i)
+  }
+}
